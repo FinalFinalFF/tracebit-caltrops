6c23f063957d3c3f999c098eede8b09aebaa1b72
Peter Baker <pete@baker.co>
2026-03-17 01:00:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js?module";
 
 // Tracebit Caltrops 3D Visualizer
 // Three axis-aligned arms with flat graphic rendering.
